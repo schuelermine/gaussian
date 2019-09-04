c76d7cd190d672a6ced07a9e26f05dde7149cd9e
Anselm Schüler <nitrogenfan@gmail.com>
2019-09-04 11:56:22 +0200

    Create Gaussian.hs

---
+++ Data/Gaussian.hs
@@ -0,0 +1,24 @@
+data Gaussian = (:+)
+  {
+    real :: Integer
+    imaginary :: Integer
+  } deriving
+    (Show, Read, Eq)
+
+instance Num Gaussian
+  where
+    (a1 :+ b1) + (a2 :+ b2) = (a1 + a2) :+ (b1 + b2)
+    (a1 :+ b1) - (a2 :+ b2) = (a1 - a2) :+ (b1 - b2)
+    (a1 :+ b1) * (a2 :+ b2) = (a1 * a2 - b1 * b2) :+ (a1 * b2 + a2 * b1)
+    abs (a :+ b) = (a ^ two) + (b ^ two)
+      where
+        two = 2 :: Integer
+    signum = id
+    negate (a :+ b) = negate a :+ negate b
+    fromInteger x = x :+ 0
+
+conjugate :: Gaussian -> Gaussian
+conjugate (a :+ b) = (a :+ negate b)
+
+i :: Gaussian
+i = 0 :+ 1
