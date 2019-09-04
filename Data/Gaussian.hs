e2bfd7cb5abac0ff6b63c196a64ecbfd4e7b1d48
Anselm Schüler <anselmschueler@gmail.com>
2019-09-04 20:15:30 +0200

    more utility functions and slight edits

--- Data/Gaussian.hs
+++ Data/Gaussian.hs
@@ -21,9 +21,6 @@ instance Num Gaussian
     negate (a :+ b) = negate a :+ negate b
     fromInteger x = x :+ 0
 
-conjugate :: Gaussian -> Gaussian
-conjugate (a :+ b) = (a :+ negate b)
-
 i :: Gaussian
 i = 0 :+ 1
 
@@ -32,11 +29,28 @@ magnitude (a :+ b) = sqrt $ (fromInteger a ^ two) + (fromInteger b ^ two)
   where
     two = 2 :: Integer
 
+norm :: Gaussian -> Integer
+norm (a :+ b) = (a ^ two) + (b ^ two)
+  where
+    two = 2 :: Integer
+
+conjugate :: Gaussian -> Gaussian
+conjugate (a :+ b) = (a :+ negate b)
+
 rotate_right :: Gaussian -> Gaussian
-rotate_right = (* (-i))
+rotate_right (a :+ b) = (b :+ negate a)
 
 rotate_left :: Gaussian -> Gaussian
-rotate_left = (* i)
+rotate_left (a :+ b) = (negate b :+ a)
+
+flip_x :: Gaussian -> Gaussian
+flip_x (a :+ b) = (negate a :+ b)
+
+flip_y :: Gaussian -> Gaussian
+flip_y = conjugate
+
+swap_x_y :: Gaussian -> Gaussian
+swap_x_y (a :+ b) = (b :+ a)
 
 toComplex :: Num p => Gaussian -> Complex.Complex p
 toComplex (a :+ b) = (fromInteger a Complex.:+ fromInteger b)
